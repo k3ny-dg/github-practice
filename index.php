b1112b54df0ed26830f5de79d425125c165503cf
Keny Dutton-Gillespie <dutton-gillespie.keny@student.greenriver.edu>
2022-04-07 12:54:20 -0400

    added divs for organization

--- index.php
+++ index.php
@@ -5,8 +5,12 @@
   <title>Git Practice!</title>
 </head>
 <body>
-<h2><strong>This is our github practice!</strong></h2>
-    <h5>I think I understand this?</h5>
-    <p>Cool! This is working!</p>
+    <div id="main">
+        <div id="page_heading">
+            <h2><strong>This is our github practice!</strong></h2>
+        </div>
+        <h5>I think I understand this?</h5>
+        <p>Cool! This is working!</p>
+    </div>
 </body>
 </html>
